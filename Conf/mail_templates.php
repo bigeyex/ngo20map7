ef622e923993cf0622f4782fb560663adb7693c8
bigeyex <wangyu@dhcp-18-111-3-115.dyn.mit.edu>
2014-10-01 18:19:03 -0400

    better retrieve password

--- Conf/mail_templates.php
+++ Conf/mail_templates.php
@@ -4,13 +4,13 @@ return array(
 
     'forget_password_email_tmpl' => <<<EndOfFile
 
-您收到这封邮件是因为您使用了公益地图(www.ngo20map.com)的找回密码功能。
-请点击一下链接找回密码：
-
-<a href="{{link}}">{{link}}</a>
-
-如果您没有使用过找回密码功能，请忽略它。
-如果您有其他的问题，请联系<a href="mailto:info@ngo20.org">NGO20</a>
+您收到这封邮件是因为您使用了公益地图(www.ngo20map.com)的找回密码功能。<br/>
+请点击一下链接找回密码：<br/>
+<br/>
+<a href="{{link}}">{{link}}</a><br/>
+<br/>
+如果您没有使用过找回密码功能，请忽略它。<br/>
+如果您有其他的问题，请联系<a href="mailto:info@ngo20.org">NGO20</a><br/>
 
 
 
