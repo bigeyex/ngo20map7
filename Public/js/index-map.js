3b072caf06746a87353a8988ac6bbedff4c372d5
Wang Yu <wangyu@dhcp-18-111-9-59.dyn.mit.edu>
2015-01-16 16:11:10 -0500

    mobile page for search result
    added mobile page for search result

--- Public/js/index-map.js
+++ Public/js/index-map.js
@@ -1,3 +1,30 @@
+var Environment = {
+    //mobile or desktop compatible event name, to be used with '.on' function
+    TOUCH_DOWN_EVENT_NAME: 'mousedown touchstart',
+    TOUCH_UP_EVENT_NAME: 'mouseup touchend',
+    TOUCH_MOVE_EVENT_NAME: 'mousemove touchmove',
+    TOUCH_DOUBLE_TAB_EVENT_NAME: 'dblclick dbltap',
+
+    isAndroid: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    isBlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    isIOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    isOpera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    isWindows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    isMobile: function() {
+        return (Environment.isAndroid() || Environment.isBlackBerry() || Environment.isIOS() || Environment.isOpera() || Environment.isWindows());
+    }
+};
+
 $(function(){
     $(window).resize(function(){
         var height = $(window).height();
@@ -14,7 +41,9 @@ $(function(){
    $(window).resize();
     
     // init all modules
-    mapView.init();
+    if(!Environment.isMobile()){
+        mapView.init();
+    }
     filterView.init();
 });
 
