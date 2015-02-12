d5f21eaca9ac02e5b2d193d54a1e81d10a3d6869
Wang Yu <bigeyex@gmail.com>
2015-02-12 15:49:25 -0500

    some translations

---
+++ Lang/en/index.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+    '找省份或城市' => 'Where?',
+    '找领域' => 'What Area?',
+    '找机构或企业' => 'What Type?',
+    '搜热词（如：留守儿童）' => 'Keywords...',
+    '搜索' => 'Search',
+    
+    );
